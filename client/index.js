fd3b5f379809754fc5b574e557d1cf4710b32ef8
Mahesh <mahesh@beehively.com>
2018-01-23 11:19:04 +0530

    Added webpack loaders

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
 import './utils/responsive';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/index.scss';
 
 console.log('Hello pack!');
--- webpack/webpack.shared.config.js
+++ webpack/webpack.shared.config.js
@@ -1,8 +1,9 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
-
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const distPath = path.resolve(__dirname, '../public/dist');
+const devEnv = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: ['./client/index.js'],
@@ -13,11 +14,85 @@ module.exports = {
     publicPath: '/dist'
   },
 
+  resolve: {
+    extensions: [".js", ".css", ".scss"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: devEnv,
+              },
+            },
+            { loader: 'sass-loader',
+              options: {
+                sourceMap: devEnv,
+              },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            options: {
+              sourceMap: devEnv,
+            },
+          },
+        }),
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      },
+      /* {
+        test: /\.(png|jpg)$/,
+        exclude: /node_modules/,
+        loader: 'url-loader?limit=5000',
+      }, */
+    ],
+  },
+
 
   plugins: [
     new CleanWebpackPlugin(distPath, {
       root: path.resolve(__dirname, '../')
+    }),
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      disable: false,
+      allChunks: true,
     })
+
   ]
 
 };
